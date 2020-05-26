2e31d6796267dbe680fae0336791c8d25a9c4977
henry-654321 <xiangkang_huang@126.com>
2020-05-26 14:08:23 +0800

    1.main.c增加设置PB0和PB1 2.增加uart.c

--- STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Project/STM32F4xx_StdPeriph_Examples/GPIO/GPIO_IOToggle/main.c
+++ STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Project/STM32F4xx_StdPeriph_Examples/GPIO/GPIO_IOToggle/main.c
@@ -71,6 +71,15 @@ int main(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOG, &GPIO_InitStructure);
 
+
+  /* Configure PB0 and PB1 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0| GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
   /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory. 
      You can monitor PG6 or PG8 on the scope to measure the output signal. 
      If you need to fine tune this frequency, you can add more GPIO set/reset 
